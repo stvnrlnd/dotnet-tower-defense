29db9e1ef6701a41215b1a5e905159554093f05a
Steven Roland <steven@mojave.io>
2020-03-01 15:15:45 -0500

    Add Path

--- Game.cs
+++ Game.cs
@@ -10,7 +10,23 @@ namespace TowerDefense
 
             try
             {
-                MapLocation point = new MapLocation(4, 2, map);
+                Path path = new Path(
+                    new [] {
+                        new MapLocation(0, 2, map),
+                        new MapLocation(1, 2, map),
+                        new MapLocation(2, 2, map),
+                        new MapLocation(3, 2, map),
+                        new MapLocation(4, 2, map),
+                        new MapLocation(5, 2, map),
+                        new MapLocation(6, 2, map),
+                        new MapLocation(7, 2, map)
+                    }
+                );
+                MapLocation location = path.GetLocationAt(0);
+                if (location != null)
+                {
+                    Console.WriteLine($"{location.X},{location.Y}");
+                }
             }
             catch(OutOfBoundsException ex)
             {
